a291b1b4b4b436433e59b592a1dfd804d7aaff73
ttbauer4 <trenton.bauer@gmail.com>
2022-07-25 15:22:56 -0500

    made excel output dynamic

--- wripli-selenium-crawler.py
+++ wripli-selenium-crawler.py
@@ -11,21 +11,25 @@ __version__ = '1.0.0'
 __email__ = 'trenton.bauer@gmail.com'
 __status__ = 'Development'
 
-import time
 import pandas as pd
 from time import localtime, strftime
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
 from openpyxl import load_workbook
-from openpyxl.workbook import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
 
-driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
+# make Firefox headless
+fireFoxOptions = webdriver.FirefoxOptions()
+fireFoxOptions.headless = True
+
+# create new Firefox driver and retreive login site
+driver = webdriver.Firefox(options = fireFoxOptions, service=Service(GeckoDriverManager().install()))
 driver.get('https://wripli.com/Account/Login?ReturnUrl=%\2F&id=loginForm')
-time.sleep(3)
 
+# login to Wripli site
 username = driver.find_element(By.XPATH, '//input[@id="Email"]')
 username.clear()
 username.send_keys('trenton.bauer@gmail.com')
@@ -37,26 +41,26 @@ password.send_keys('P@ssw0rd')
 submit= driver.find_element(By.XPATH, '//input[@type="submit"]')
 submit.click()
 
+# driver at homepage; create new bs object
 content = driver.page_source
-soup=BeautifulSoup(content,features="lxml")
+soup=bs(content,features="lxml")
 
 timestamp = []
+row_headers = ['Assigned Valves: Online', 'Assigned Valves: Offline', 'Assigned Valves: Inactive', 'Unassigned Valves', 'Total Dealers']
 homepage_titlecard_values = []
 for x in soup.findAll('h5'):
     homepage_titlecard_values.append(x.text)
     timestamp.append(strftime("%Y-%m-%d %H:%M:%S", localtime()))
 
-
 filePath = 'WripliData.xlsx'
-workbook = load_workbook(filePath)
-
-row_headers = ['Assigned Valves: Online', 'Assigned Valves: Offline', 'Assigned Valves: Inactive', 'Unassigned Valves', 'Total Dealers']
+wb = load_workbook(filePath)
 writer = pd.ExcelWriter(filePath, engine = 'openpyxl')
-writer.book = workbook
+writer.book = wb
 
-df = pd.DataFrame({'Time (YYYY-MM-DD HH:MM:SS)' : timestamp ,'Category' : row_headers , 'Values' : homepage_titlecard_values})
-df.to_excel(writer, filePath, index=False, encoding='utf-8')
+df1 = pd.DataFrame({'Time (YYYY-MM-DD HH:MM:SS)' : timestamp ,'Category' : row_headers , 'Values' : homepage_titlecard_values})
+
+df1.to_excel(writer, sheet_name=strftime("%Y-%m-%d %H.%M.%S", localtime()))
+
 writer.save()
 writer.close()
-
 driver.close()
