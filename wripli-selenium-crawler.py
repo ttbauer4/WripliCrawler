12786834393ab89e6aa8568e5855db4524996e43
ttbauer4 <trenton.bauer@gmail.com>
2022-07-25 10:15:53 -0500

    prototype selenium crawler

--- wripli-selenium-crawler.py
+++ wripli-selenium-crawler.py
@@ -12,10 +12,13 @@ __email__ = 'trenton.bauer@gmail.com'
 __status__ = 'Development'
 
 import time
+import pandas as pd
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
+from openpyxl.workbook import Workbook
 
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 driver.get('https://wripli.com/Account/Login?ReturnUrl=%\2F&id=loginForm')
@@ -32,4 +35,15 @@ password.send_keys('P@ssw0rd')
 submit= driver.find_element(By.XPATH, '//input[@type="submit"]')
 submit.click()
 
+content = driver.page_source
+soup=BeautifulSoup(content,features="lxml")
 
+homepage_titlecard_values = []
+for x in soup.findAll('h5'):
+    homepage_titlecard_values.append(x.text)
+
+row_headers = ['Assigned Valves: Online', 'Assigned Valves: Offline', 'Assigned Valves: Inactive', 'Unassigned Valves', 'Total Dealers']
+df = pd.DataFrame({'' : row_headers , 'Values' : homepage_titlecard_values})
+df.to_excel('data.xlsx', index=False, encoding='utf-8')
+
+driver.close()
