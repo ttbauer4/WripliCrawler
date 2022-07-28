9283aca623457665c0cf78acfb8d1e87390ea010
ttbauer4 <trenton.bauer@gmail.com>
2022-07-28 13:51:48 -0500

    private.py template V1.0.1

---
+++ private_TEMPLATE.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+''' private.py: defines private fields for dealer-crawler.py
+'''
+
+# TODO: fill metadata
+__author__ = '<YOUR NAME>'
+__version__ = 'V1.0.1'
+__email__ = '<YOUR EMAIL>'
+__status__ = '<STATUS>'
+
+# TODO: Dealer Email and Password
+dealerUser = '<EMAIL>'
+dealerPass = '<PASSWORD>'
+
+# TODO: Consumer Email and Password
+consumerUser = '<EMAIL>'
+consumerPass = '<PASSWORD>'
+
+# Test Units
+macArray = []
+
+### TODO: APPEND MAC ADDRESSES HERE ###
+
+# TODO: dataset file location
+wdFilePath = '<FILE PATH>'
+
+# TODO: dataset file name
+wdFileName = '<FILE NAME>'
+
+# login page URL
+loginURL = '<LOGIN URL>'
+
+# home page URL
+homeURL = '<HOME URL>'
+
+# unit page URL prefix
+unitURL = '<UNIT URL PREFIX>'
