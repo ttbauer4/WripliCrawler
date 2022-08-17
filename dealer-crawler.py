2c6f9f2e9fa8b3e19f60e38dc48b0b78bc1e0589
ttbauer4 <trenton.bauer@gmail.com>
2022-08-17 14:42:41 -0500

    improved style

--- dealer-crawler.py
+++ dealer-crawler.py
@@ -24,7 +24,6 @@ from datetime import datetime
 from time import localtime, strftime
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
-
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.common.by import By
